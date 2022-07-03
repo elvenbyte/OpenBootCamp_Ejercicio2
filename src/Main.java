8c04ab3cb438a1fed47ae8531d555d141ac53c91
David Moñino <damobe@gmail.com>
2022-07-03 12:41:53 +0200

    ejercicio listo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-	System.out.println("Hello world!");
+	int numeroIf = 0;
+	int numeroWhile = 0;
+	String estacion = "invierno";
+
+	if(numeroIf < 0){
+	    System.out.println("numeroIf es negativo");
+	} else if (numeroIf > 0) {
+	    System.out.println("numeroIf es positivo");
+	}else{
+	    System.out.println("numeroIf es cero");
+	}
+
+	while(numeroWhile < 3){
+	    numeroWhile++;
+	    System.out.println(numeroWhile);
+	}
+
+	do {
+	    numeroWhile++;
+	    System.out.println(numeroWhile);
+	}while(numeroWhile == 3);
+
+	for(int numeroFor = 0; numeroFor <= 3; numeroFor++){
+	    System.out.println(numeroFor);
+	}
+
+	switch (estacion){
+	case "invierno":
+	    System.out.println(estacion);
+	    break;
+	case "primavera":
+	    System.out.println(estacion);
+	    break;
+	case "verano":
+	    System.out.println(estacion);
+	    break;
+	case "otoño":
+	    System.out.println(estacion);
+	    break;
+	default:
+	    System.out.println("no es una estación");
+	}
     }
 }
